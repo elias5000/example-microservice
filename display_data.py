e25435c10345584302a841d7510c086105cc5c2c
Frank Wittig <frank.wittig@here.com>
2017-01-19 13:36:40 +0100

    initial commit

---
+++ display_data.py
@@ -0,0 +1,103 @@
+# coding=utf-8
+
+import BaseHTTPServer
+import os
+import urllib2
+from time import asctime
+from urlparse import urlparse
+import json
+
+# Config parameters to be set in task definition
+port = int(os.environ['PORT'])
+url = "http://%s:%d/?q=%%s" % (os.environ['API_HOST'], int(os.environ['API_PORT']))
+server_type = os.environ['TYPE']
+
+page = '''
+<html>
+    <header>
+        <title>Weather API Demo</title>
+        <style type="text/css">
+            #data {
+                background: #fff;
+                border: 3px solid black;
+                border-radius: 25px;
+                width: 300px;
+                margin: auto;
+                text-align: center;
+                font-size: 50px;
+                font-family: sans-serif;
+            }
+
+            h1 {
+                font-size: 35px;
+                background: #ccc;
+            }
+        </style>
+    </header>
+    <body>
+        <div id="data">
+            <h1>%s</h1>
+            <p>%s</p>
+        </div>
+    </body>
+</html>
+'''
+
+
+class ReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def params(self):
+        try:
+            query = urlparse(self.path).query
+            return dict(qc.split("=") for qc in query.split("&"))
+        except:
+            return dict()
+
+    def do_HEAD(self):
+        self.do_GET()
+
+    def do_GET(self):
+        try:
+            city = self.params()['q']
+        except KeyError:
+            city = "Berlin"
+
+        try:
+            get_data(city)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(page % (city, get_data(city)))
+
+        except Exception as e:
+            print asctime(), e
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write('500 - Could not respond your request.')
+
+
+def get_data(city):
+    doc = urllib2.urlopen(url % city).read()
+
+    if server_type == 'temp':
+        return "%.1f°C" % float(json.loads(doc)['main']['temp'] - 273.15)
+    elif server_type == 'pressure':
+        return "%d hPa" % int(json.loads(doc)['main']['pressure'])
+    elif server_type == 'wind':
+        return "%.1f m/s" % float(json.loads(doc)['wind']['speed'])
+
+
+def run(server_class=BaseHTTPServer.HTTPServer, handler_class=ReqHandler):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print asctime(), "Server starts on port %d" % port
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print asctime(), "Server shutting down"
+    httpd.server_close()
+    print asctime(), "Server stopped"
+
+
+if __name__ == '__main__':
+    run()
---
+++ get_weather.py
@@ -0,0 +1,56 @@
+import BaseHTTPServer
+import os
+import urllib2
+from time import asctime
+from urlparse import urlparse
+
+# Config parameters to be set in task definition
+apikey = os.environ['APIKEY']
+port = int(os.environ['PORT'])
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={%%s}&APPID=%s' % apikey
+
+
+class ReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def params(self):
+        try:
+            query = urlparse(self.path).query
+            return dict(qc.split("=") for qc in query.split("&"))
+        except:
+            return dict()
+
+    def do_HEAD(self):
+        self.do_GET()
+
+    def do_GET(self):
+        try:
+            city = self.params()['q']
+        except KeyError:
+            city = "Berlin"
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/json')
+        self.end_headers()
+        self.wfile.write(fetch_data(city))
+
+
+def fetch_data(city):
+    doc = urllib2.urlopen(url % city).read()
+    return doc
+
+
+def run(server_class=BaseHTTPServer.HTTPServer, handler_class=ReqHandler):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print asctime(), "Server starts on port %d" % port
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print asctime(), "Server shutting down"
+    httpd.server_close()
+    print asctime(), "Server stopped"
+
+
+if __name__ == '__main__':
+    run()
