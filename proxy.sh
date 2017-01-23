dae4322989e2eb0a14c0424d5330cbd397d81fe4
Frank Wittig <frank.wittig@here.com>
2017-01-23 11:49:03 +0100

    Add basic auth config

--- proxy.sh
+++ proxy.sh
@@ -2,5 +2,7 @@
 sed -i "s/__env_port__/$PORT/g" /etc/nginx/conf.d/proxy.conf
 sed -i "s/__env_upstream__/$UPSTREAM/g" /etc/nginx/conf.d/proxy.conf
 
+htpasswd -bc /etc/nginx/htpasswd $USER $PASSWD
+
 nginx \
   -g "daemon off; error_log /dev/stdout info;"
